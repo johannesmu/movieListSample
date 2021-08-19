90836030d647a36c26ab6b9d2bfb258e3577ad84
DaniYumi <8477@ait.nsw.edu.au>
2021-08-19 21:24:15 +1000

    firabase config

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYvG6t8xSXCFNeOQsh32DJMTmL6FgSFzo",
+    authDomain: "assessment2-6fba0.firebaseapp.com",
+    projectId: "assessment2-6fba0",
+    storageBucket: "assessment2-6fba0.appspot.com",
+    messagingSenderId: "1019574675026",
+    appId: "1:1019574675026:web:62505865336ace6cc9218b"
+  };
