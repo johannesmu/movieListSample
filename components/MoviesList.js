7d53ac5b9a9e36c2e923b52c96998612f61d8106
DaniYumi <8477@ait.nsw.edu.au>
2021-08-19 21:34:53 +1000

    movies list

---
+++ components/MoviesList.js
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { Logout } from './Logout';
+import { Logo } from './Logo';
+import { Footer } from './Footer';
+
+export function MoviesList(props){
+    const [user, setUser]=useState()
+
+    const navigation = useNavigation()
+
+    navigation.setOptions({
+        headerRight:()=>(
+            <Logout handler={signOut} />
+        )
+    })
+    
+    useEffect( () => {
+        if( props.auth ) {
+          setUser( props.auth )
+        }
+        else {
+          setUser(null)
+        }
+        navigation.setOptions({
+          headerRight: props => <Logout {...props} handler={signOut}/>
+        })
+      })
+    
+    
+      const Greeting = () => {
+        if(!user) {
+          return null
+        }
+        else{
+          return <Text>{user.email}</Text>
+        }
+      }
+    const signOut=() =>{
+        props.signout().then((result)=>{
+            if(result === true ){
+                navigation.reset({ index: 0, routes: [{ name: "Signin" }] })
+            }
+        })
+       .catch((error)=> console.log(error))
+    }
+    const Renderer = ({list}) => (
+
+      
+        <View style={ (list.status === true) ? ListStyle.listItemDone : ListStyle.listItem}>
+            
+          <View style={ListStyle.buttons}>   
+            <TouchableOpacity onPress={ () => props.doneHandler(list.id) }>
+            <Text style={ListStyle.buttonText}>Done</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={ () => props.deleteHandler(list.id) }>
+            <Text style={ListStyle.buttonText1}>Delete</Text>
+            </TouchableOpacity>
+            <Text style={ListStyle.listText}>{list.name}</Text>
+          </View>
+        </View>
+    )
+   
+    return(
+        <View style={ListStyles.page}> 
+            <Greeting />
+            <TouchableOpacity onPress={ signOut}>
+            </TouchableOpacity>
+            <Text style={ListStyles.headTitle}>Movies List</Text>
+            <Logo/>
+            <FlatList     data={ props.read } 
+            renderList={ Renderer} 
+            keyExtractor={ list => list.id.toString()}/>
+            <Footer/>
+        </View>
+    )
+}
+const ListStyles= StyleSheet.create({
+
+    page:{
+        fontSize: 26,
+        backgroundColor: '#faf7c7',
+        height:1000,
+    },
+    headTitle:{
+        fontSize: 26,
+        textAlign:'center',
+        marginTop:20,
+        marginBottom:20,
+        color: '#263e47',
+    },
+   
+})
